6c1b9fee27257d69e4f943e540324e30ab380d35
Michał Drab <michal.drab95@gmail.com>
2020-01-11 23:58:56 +0100

    Fix general controller

--- src/main/java/com/weatherstation/controller/MainController.java
+++ src/main/java/com/weatherstation/controller/MainController.java
@@ -1,19 +1,20 @@
 package com.weatherstation.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
     public MainController() {
     }
-    @GetMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String mainController(){
-        return "<html><title>Stacja pogodowa - info</title><body>"
+        return "<html><title>Weather Station - info</title><body>"
                 + "<h2>General information</h2>"
                 + "<p>Encrypt your thingspeak password with com.weatherstation.util.PasswordEncrypter</p>"
                 + "<p>Edit com.weatherstation.WeatherstationApplication - set your encrypted thingspeak password and list of sensors</p>"
-                + "<p>build project with mvn clean install command</p>"
+                + "<p>build project with 'mvn clean install' command</p>"
                 + "</body></html>";
     }
 }
