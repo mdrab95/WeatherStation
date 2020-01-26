2cd541d9ccdb9626e0d473fa173963dc20326e1f
Michał Drab <michal.drab95@gmail.com>
2020-01-26 16:59:05 +0100

    Password encryption from http page

--- src/main/java/com/weatherstation/controller/MainController.java
+++ src/main/java/com/weatherstation/controller/MainController.java
@@ -1,18 +1,50 @@
 package com.weatherstation.controller;
 
+import com.weatherstation.util.PasswordDecrypter;
+import org.jasypt.util.text.BasicTextEncryptor;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class MainController {
+
+    String encryptedPassword;
+
     public MainController() {
+        encryptedPassword = "";
     }
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String mainController(){
-        return "<html><title>Weather Station - info</title><body>"
-                + "<h2>General information</h2>"
-                + "<p>https://github.com/mdrab95/WeatherStation</p>"
-                + "</body></html>";
+    public ModelAndView home() {
+        return new ModelAndView("index");
+    }
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public ModelAndView index() {
+        return new ModelAndView("index");
+    }
+
+    @RequestMapping(value = "/password", method = RequestMethod.GET)
+    public ModelAndView password() {
+        return new ModelAndView("password");
+    }
+
+    @RequestMapping(value = "/password", method = RequestMethod.POST)
+    public ModelAndView encryptPassword(@RequestParam String password, @RequestParam String appPassword, Model model) {
+        BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+        textEncryptor.setPasswordCharArray(appPassword.toCharArray());
+        encryptedPassword = textEncryptor.encrypt(password);
+        model.addAttribute("password", encryptedPassword);
+        return new ModelAndView("encryptedPassword");
+    }
+
+    @RequestMapping(value = "/encryptedpassword", method = RequestMethod.GET)
+    public ModelAndView showEncryptedPassword(Model model) {
+        model.addAttribute("password", encryptedPassword);
+        return new ModelAndView("encryptedpassword");
     }
 }
